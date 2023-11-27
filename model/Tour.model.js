9894e51f4068e9d6d2e3b9353551c1f513ff1adb
Imran Biswas <imranbiswas2209@gmail.com>
2023-11-27 22:32:59 +0530

    Add topur model.

--- model/Tour.model.js
+++ model/Tour.model.js
@@ -7,7 +7,61 @@ const tourSchema = new Schema(
       required: [true, "A tour must've a name!"],
       unique: true,
     },
+
+    duration: {
+      type: String,
+      required: [true, "A tour must've a duration!"],
+    },
+
+    difficulty: {
+      type: String,
+      required: [true, "A tour must've a duration!"],
+    },
+
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+
+    maxGroupSize: {
+      type: Number,
+      required: [true, "A tour must've a duration!"],
+    },
+
+    price: {
+      type: Number,
+      required: [true, "A tour must've a duration!"],
+    },
+
+    priceDiscount: Number,
+
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, "warning"],
+    },
+
+    description: {
+      type: String,
+      trim: true,
+      required: [true, "warning"],
+    },
+
+    imageCover: {
+      type: String,
+      required: [true, "warning"],
+    },
+
+    images: [String],
+
+    startDates: [Date],
   },
+
   { timestamps: true }
 );
 
