5cc737936b7dbc3f552325e9ecd5881a088c15e9
Imran Biswas <imranbiswas2209@gmail.com>
2024-07-26 16:45:49 +0530

    chore: housekeeping

--- app.js
+++ app.js
@@ -28,11 +28,6 @@ app.enable('trust proxy');
 // 1) GLOBAL MIDDLEWARES
 // Implement CORS
 app.use(cors());
-// Access-Control-Allow-Origin *
-// api.natours.com, front-end natours.com
-// app.use(cors({
-//   origin: 'https://www.natours.com'
-// }))
 
 app.options('*', cors());
 // app.options('/api/v1/tours/:id', cors());
